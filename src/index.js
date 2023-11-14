9e5903aef34414311abea88bd7b2b25532a97c51
Tanguy Krotoff <tkrotoff@gmail.com>
2023-11-14 21:09:24 +0100

    Switch to CJS + createElement instead of JSX
    Because Microbundle and Rollup don't support 'use client'

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useEffect, useRef } from 'react'
+const { createElement, useEffect, useRef } = require('react')
 
 function DangerouslySetHtmlContent({ html, dangerouslySetInnerHTML, ...rest }) {
   // We remove 'dangerouslySetInnerHTML' from props passed to the div
@@ -17,8 +17,7 @@ function DangerouslySetHtmlContent({ html, dangerouslySetInnerHTML, ...rest }) {
     divRef.current.appendChild(slotHtml) // Append the new content
   }, [html, divRef])
 
-  // eslint-disable-next-line react/react-in-jsx-scope
-  return <div {...rest} ref={divRef} />
+  return createElement('div', { ...rest, ref: divRef })
 }
 
-export default DangerouslySetHtmlContent
+module.exports = DangerouslySetHtmlContent
